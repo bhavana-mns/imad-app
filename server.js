8d605d8b1b3d412f2381b464a6d2ab87f0f5e91e
bhavana-mns <mnsbha123@gmail.com>
2017-08-04 21:36:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,39 +5,59 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne={
-  title: "Article One | Bhavana Malepaty",
-  heading: "Article One",
-  date: "Aug 4 2017",
-  content: `
-                <p>
-                    This is the content for my first article. This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                </p>
-                <p>
-                    This is the content for my first article. This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                </p>
-                <p>
-                    This is the content for my first article. This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
-                </p>
-            `
+var articles = {
+    articleOne:{
+      title: "Article One | Bhavana Malepaty",
+      heading: "Article One",
+      date: "Aug 4 2017",
+      content: `
+                    <p>
+                        This is the content for my first article. This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                    </p>
+                    <p>
+                        This is the content for my first article. This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                    </p>
+                    <p>
+                        This is the content for my first article. This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
+                    </p>
+                `
+    },
+    articleTwo:{
+      title: "Article Two | Bhavana Malepaty",
+      heading: "Article Two",
+      date: "Aug 4 2017",
+      content: `
+                    <p>
+                        This is the content for my second article. 
+                    </p>
+                    <p>
+                        This is the content for my second article. 
+                    </p>
+                    <p>
+                        This is the content for my second article. 
+                    </p>
+                `
+    },
+    articleThree:{
+      title: "Article Three | Bhavana Malepaty",
+      heading: "Article Three",
+      date: "Aug 4 2017",
+      content: `
+                    <p>
+                        This is the content for my third article. 
+                    </p>
+                    <p>
+                        This is the content for my third article. 
+                    </p>
+                    <p>
+                        This is the content for my third article. 
+                    </p>
+                `
+    }
 };
 
-var articleTwo={
-  title: "Article Two | Bhavana Malepaty",
-  heading: "Article Two",
-  date: "Aug 4 2017",
-  content: `
-                <p>
-                    This is the content for my second article. 
-                </p>
-                <p>
-                    This is the content for my second article. 
-                </p>
-                <p>
-                    This is the content for my second article. 
-                </p>
-            `
-};
+
+
 var articleThree={
   title: "Article Three | Bhavana Malepaty",
   heading: "Article Three",
