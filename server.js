6a11fbb3dd1ceb0aefdc30617dd2394f3f2c9bc4
bhavana-mns <mnsbha123@gmail.com>
2017-08-05 12:44:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,10 +94,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
 var counter = 0;
 app.get('/counter', function (req, res) {
   counter=counter+1;
@@ -111,6 +107,10 @@ app.get('/:articleName', function(req,res){
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
