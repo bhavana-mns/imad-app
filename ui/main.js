48d09dc8c1975626d34911621fdb5004ff97c4d4
bhavana-mns <mnsbha123@gmail.com>
2017-08-05 12:30:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,3 +14,12 @@ function moveRight(){
 img.onclick=function(){
   var interval = setInterval(moveRight,50);
 };
+
+//Counter code
+var button=document.getElementById('counter');
+button.onclick=function(){
+  
+  counter=counter+1;
+  var span=document.getElementById('count');
+  span.innerHTML=counter.toString();
+};
