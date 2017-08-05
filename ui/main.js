e2a178ef3dc0a1cc5be18ad28bfcd0d4fc766fae
bhavana-mns <mnsbha123@gmail.com>
2017-08-05 12:35:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,4 @@
-console.log('Loaded!');
 
-//Change contents of main-text div
-var element = document.getElementById('main-text');
-//element.innerHTML='New value';
-
-//Move the image
-var img=document.getElementById('madi');
-var marginLeft = 0;
-function moveRight(){
-    marginLeft+=1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-/**img.onclick=function(){
-  var interval = setInterval(moveRight,50);
-};**/
 
 //Counter code
 var button=document.getElementById('counter');
