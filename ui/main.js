dba47088df1ce4c795c539239fa123e5f2a998ac
bhavana-mns <mnsbha123@gmail.com>
2017-08-06 10:20:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,8 +4,9 @@
 var button1=document.getElementById('counter');
 var counts=0;
 
-button1.onclick = function(){
+button1.onclick = startCount;
+function startCount(){
   counts=counts+1;
   var span=document.getElementById('count');
   span.innerHTML=counts.toString();
-};
+}
