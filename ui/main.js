82ba6c86b257ca93148fb90407cd49a3a216ec01
bhavana-mns <mnsbha123@gmail.com>
2017-08-06 11:13:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-
+/**
 //Change contents of main-text div
  var element = document.getElementById('main-text').innerHTML='New value';
  
@@ -14,14 +14,25 @@
    var interval = setInterval(moveRight,50);
  };
   
-
+**/
 
 //Counter code
 var button=document.getElementById("counter");
-var counter=0;
+//var counter=0;
 
-button.onclick = function startCount(){
-  counter=counter+1;
-  var span=document.getElementById('count');
-  span.innerHTML=counter.toString();
+button.onclick = function(){
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readystate === XMLHttpRequest.DONE){
+            if(request.status===200){
+                var counter = request.responseText;
+            }
+        }
+    }
+    
+    /**counter=counter+1;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();**/
 };
