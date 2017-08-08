9a99af6a7e1f73811e0bd2a4e1a3302de15c9815
bhavana-mns <mnsbha123@gmail.com>
2017-08-08 19:06:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -88,15 +88,15 @@ function createTemplate(data){
     
     return htmlTemplate;
 }
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 
 var counter = 0;
 app.get('/counter', function (req, res) {
