1d805b9c952f9a87a66f1cd230be400af6103498
bhavana-mns <mnsbha123@gmail.com>
2017-08-08 18:35:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,6 +43,11 @@ button.onclick = function(){
 //}; 
 
 //Submit Name
-
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn')
+submit.onclick = function(){
+    
+}
 
 
