708ac75d0a56d5acdc612a64cc81531f017d075f
bhavana-mns <mnsbha123@gmail.com>
2017-08-08 19:02:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,4 @@
+window.onload = function(){
 /**
 //Change contents of main-text div
  var element = document.getElementById('main-text').innerHTML='New value';
@@ -13,9 +14,9 @@
  img.onclick=function(){
    var interval = setInterval(moveRight,50);
  };
-  
-**/
-/**
+ **/ 
+
+
 //Counter code
 var button=document.getElementById('counter');
 //var counter=0;
@@ -40,8 +41,8 @@ button.onclick = function(){
     /**counter=counter+1;
     var span=document.getElementById('count');
     span.innerHTML=counter.toString();**/
-//}; 
-window.onload = function(){
+}; 
+
 //Submit Name
 var nameInput = document.getElementById('name');
 //var name = nameInput.value();
