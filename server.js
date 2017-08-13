1cda94293c13f7c1bdf7668aaea9528ccd5dfbc9
bhavana-mns <mnsbha123@gmail.com>
2017-08-13 20:15:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -144,10 +144,15 @@ app.get('/articles/:articleName', function(req,res){
         if(err){
            res.status(500).send(err.toString());
        }else{
-           res.send(JSON.stringify(result.rows));
+           if(result.rows.length===0){
+               res.status(404).send('Article not found');
+           }else{
+               var articleData=result.rows[0];
+               res.send(createTemplate(articleData));
+           }
        }
     });
-    res.send(createTemplate(articleData));
+    
 });
 
 app.get('/ui/style.css', function (req, res) {
