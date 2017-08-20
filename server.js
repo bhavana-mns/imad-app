66026e9678028d2c34ef1dbf1151c503b24da951
bhavana-mns <mnsbha123@gmail.com>
2017-08-20 17:26:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -179,8 +179,8 @@ app.get('/check-login',function(req,res){
         res.send("You are logged in: "+req.session.auth.userId.toString());
     }else{
         res.send('You are not logged in');
-    };
-})
+    }
+});
 
 var pool = new Pool(config);
 
