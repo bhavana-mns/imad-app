013f96eec21bf7488b0d873cece7a73cb79cdecb
bhavana-mns <mnsbha123@gmail.com>
2017-08-20 16:25:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -62,5 +62,28 @@ submit.onclick = function(){
   
 }; **/
 
-
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readyState===XMLHttpRequest.DONE){
+            if(request.status===200){
+                var names=request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for(var i=0;i<names.length;i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+            }
+        }
+    };
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value
+    request.open('POST','http://mnsbha12362.imad.hasura-app.io/submit-name?name='+name,true);
+    request.send(JSON.stringify({username:username,password:password}));
+}
 
